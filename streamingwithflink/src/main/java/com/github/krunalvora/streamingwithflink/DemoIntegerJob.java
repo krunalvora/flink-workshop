fc44c979c715247e1248ddd99ea7a66e377816c6
Krunal Vora <krunalvora4@gmail.com>
2021-04-10 17:38:25 +0530

    comap

--- streamingwithflink/src/main/java/com/github/krunalvora/streamingwithflink/DemoIntegerJob.java
+++ streamingwithflink/src/main/java/com/github/krunalvora/streamingwithflink/DemoIntegerJob.java
@@ -5,9 +5,12 @@ import org.apache.flink.api.common.functions.FilterFunction;
 import org.apache.flink.api.common.functions.FlatMapFunction;
 import org.apache.flink.api.common.functions.MapFunction;
 import org.apache.flink.api.common.functions.ReduceFunction;
+import org.apache.flink.api.java.tuple.Tuple2;
 import org.apache.flink.api.java.tuple.Tuple3;
+import org.apache.flink.streaming.api.datastream.ConnectedStreams;
 import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.api.functions.co.CoMapFunction;
 import org.apache.flink.util.Collector;
 
 
@@ -52,6 +55,24 @@ public class DemoIntegerJob {
     env.execute();
   }
 
+  public static void executeMultiStreamTransformations() throws Exception {
+    StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+
+    // Data source stream from elements of a list
+    DataStream<Integer> integerStreamEven = env.fromElements(2, 4, 6, 8);
+    DataStream<Integer> integerStreamOdd = env.fromElements(1, 3, 5, 7, 9);
+    DataStream<String> stringStreamOdd = env.fromElements("1", "3", "5", "7", "9");
+
+    // DataStream<Integer> outputStream = integerStreamOdd.union(integerStreamEven);
+    DataStream<Integer> outputStream = integerStreamOdd
+          .connect(stringStreamOdd)  // creates ConnectedStreams<Integer, String>
+          .map(new allInts());
+
+    System.out.println(outputStream.print());
+
+    env.execute();
+  }
+
 
   public static class IncrementByHundred implements MapFunction<Integer, Integer> {
     @Override
@@ -78,17 +99,32 @@ public class DemoIntegerJob {
     }
   }
 
+
   public static class SumReduce implements ReduceFunction<Tuple3<Integer, Integer, Integer>> {
+    @Override
+    public Tuple3<Integer, Integer, Integer> reduce(Tuple3<Integer, Integer, Integer> value1,
+                                                    Tuple3<Integer, Integer, Integer> value2) throws Exception {
+      return Tuple3.of(value1.f0, value1.f1 + value2.f1, value1.f2);
+    }
+  }
+
+  public static class allInts implements CoMapFunction<Integer, String, Integer> {
 
     @Override
-    public Tuple3<Integer, Integer, Integer> reduce(Tuple3<Integer, Integer, Integer> value1, Tuple3<Integer, Integer, Integer> value2) throws Exception {
-      return Tuple3.of(value1.f0, value1.f1 + value2.f1, value1.f2);
+    public Integer map1(Integer value) throws Exception {
+      return value;
+    }
+
+    @Override
+    public Integer map2(String value) throws Exception {
+      return Integer.parseInt(value);
     }
   }
 
 
   public static void main(String[] args) throws Exception {
     // executeBasicTransformations();
-    executeKeyedTransformations();
+    // executeKeyedTransformations();
+    executeMultiStreamTransformations();
   }
 }
