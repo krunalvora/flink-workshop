ccd38c4889a871cce5a14f39d61f7fef074fcfae
Krunal Vora <krunalvora4@gmail.com>
2021-04-10 06:35:20 +0530

    Streaming with flink first example

---
+++ streamingwithflink/src/main/java/com/github/krunalvora/streamingwithflink/util/SensorReading.java
@@ -0,0 +1,22 @@
+package com.github.krunalvora.streamingwithflink.util;
+
+public class SensorReading {
+
+  public String id;
+  public long timestamp;
+  public double temperature;
+
+  // Empty default constructor to satisfy Flink's POJO requirements
+  public SensorReading() {
+  }
+
+  public SensorReading(String id, long timestamp, double temperature) {
+    this.id = id;
+    this.timestamp = timestamp;
+    this.temperature = temperature;
+  }
+
+  public String toString() {
+    return(String.format("(%s, %d, %f)", id, timestamp, temperature));
+  }
+}
